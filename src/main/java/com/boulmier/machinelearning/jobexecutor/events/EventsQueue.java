c871690553e7db45c949f96dd5e762707837638c
anthony <anthony.boulmier.cfpt@gmail.com>
2014-11-22 15:57:30 +0100

    modif events queue

--- src/main/java/com/boulmier/machinelearning/jobexecutor/events/EventsQueue.java
+++ src/main/java/com/boulmier/machinelearning/jobexecutor/events/EventsQueue.java
@@ -6,39 +6,27 @@
 
 package com.boulmier.machinelearning.jobexecutor.events;
 
-import com.boulmier.machinelearning.jobexecutor.JobExecutor;
 import com.boulmier.machinelearning.jobexecutor.job.Job;
 import java.util.LinkedList;
-import java.util.List;
 
 /**
- * use javaSysMon to set the maximum of parrallel job
+ * use javaSysMon to set the maximum of parallel job
  * @author antho
  */
 public class EventsQueue {
     
-    private final int maxPar = JobExecutor.sysMon.numCpus();
-    private int current      = 0;
-    private final LinkedList<Job>[] queues;
+    private final LinkedList<Job> queues;
     
     public EventsQueue() {
-        queues = new LinkedList[maxPar];
+        queues = new LinkedList<>();
     }
     
     public Job next(){
-        Job j = queues[current].pollFirst();
-        current = (current+1) % maxPar;
-        return j;
+        return queues.pollFirst();
     }
     
     public void addToQueue(Job job){
-        int min = current;
-        
-        for(int i = 0;i<queues.length;i++) {
-            List l = queues[i];
-            min = l.size() < queues[min].size() ? i:min;
-        }
-        queues[min].push(job);
+        queues.push(job);
     }
     
 }
